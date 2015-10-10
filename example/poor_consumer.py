f88f79c32a3ea727af574e240a68d934b2d55dbd
canardleteer <eris.has.a.dad@gmail.com>
2015-10-10 16:28:49 -0700

    More command line options for poor_consumer.py

--- example/poor_consumer.py
+++ example/poor_consumer.py
@@ -16,7 +16,11 @@ def usage():
     poor_consumer.py [args]
 
     acceptable arguments:
-    --nack=[float 0-1 | default 0.5] - percentage we should fail
+    --nack=[float 0-1 | default 0.0] - percentage we should fail
+    --servers=[comma separated host:port list
+        defaults to: localhost:7712,localhost:7711]
+        (can be a single host)
+    --queues=[comma separated list of queues | default "test"]
     """
     print(txt)
 
@@ -24,14 +28,18 @@ def usage():
 def main():
     """Start the poor_consumer."""
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hn:v", ["help", "nack="])
+        opts, args = getopt.getopt(sys.argv[1:], "h:v", ["help", "nack=",
+                                   "servers=", "queues="])
     except getopt.GetoptError as err:
         print str(err)
         usage()
         sys.exit()
 
-    nack = 0.5
+    # defaults
+    nack = 0.0
     verbose = False
+    servers = "localhost:7712,localhost:7711"
+    queues = "test"
 
     for o, a in opts:
         if o == "-v":
@@ -39,19 +47,30 @@ def main():
         elif o in ("-h", "--help"):
             usage()
             sys.exit()
-        elif o in ("-n", "--nack"):
+        elif o in ("--nack"):
             nack = float(a)
+        elif o in ("--servers"):
+            servers = a
+        elif o in ("--queues"):
+            queues = a
         else:
             assert False, "unhandled option"
 
-    c = Client(['localhost:7712', 'localhost:7711'])
+    # prepare servers and queus for pydisque
+    servers = servers.split(",")
+    queues = queues.split(",")
+
+    c = Client(servers)
     c.connect()
 
     while True:
-        jobs = c.get_job(['test'])
+        jobs = c.get_job(queues)
         for queue_name, job_id, job in jobs:
             rnd = random.random()
-            job = json.loads(job)
+
+            # as this is a test processor, we don't do any validation on
+            # the actual job body
+
             if rnd >= nack:
                 print ">>> received job:", job
                 c.ack_job(job_id)
